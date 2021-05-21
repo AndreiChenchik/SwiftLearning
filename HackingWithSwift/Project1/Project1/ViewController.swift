616b9c1dbb9cb366ce40becd6c89a65baaa927d0
Andrei Chenchik <andrei@chenchik.me>
2021-05-21 15:36:35 +0200

    HWS: Project 1, Storm Viewer - project init

---
+++ HackingWithSwift/Project1/Project1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Andrei Chenchik on 21/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
