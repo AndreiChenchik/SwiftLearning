4f015f6fa5040e7500dd158092e84612d692cede
Andrei Chenchik <andrei@chenchik.me>
2021-05-30 11:47:38 +0200

    HWS: Enable SwiftLint for Milestone Project 7-9

--- HackingWithSwift/Milestone Project 7-9/Milestone Project 7-9/GradientView.swift
+++ HackingWithSwift/Milestone Project 7-9/Milestone Project 7-9/GradientView.swift
@@ -11,22 +11,22 @@ class GradientView: UIView {
     private let gradient: CAGradientLayer = CAGradientLayer()
     private let gradientStartColor: UIColor
     private let gradientEndColor: UIColor
-    
+
     init(gradientStartColor: UIColor, gradientEndColor: UIColor) {
         self.gradientStartColor = gradientStartColor
         self.gradientEndColor = gradientEndColor
         super.init(frame: .zero)
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func layoutSublayers(of layer: CALayer) {
         super.layoutSublayers(of: layer)
         gradient.frame = self.bounds
     }
-    
+
     override func draw(_ rect: CGRect) {
         gradient.frame = self.bounds
         gradient.colors = [gradientStartColor.cgColor, gradientEndColor.cgColor]
--- HackingWithSwift/Milestone Project 7-9/Milestone Project 7-9/ViewController.swift
+++ HackingWithSwift/Milestone Project 7-9/Milestone Project 7-9/ViewController.swift
@@ -13,8 +13,7 @@ class ViewController: UIViewController {
     var failedAttempts = [String]()
     var words: [String]?
     var scoresLabel: UILabel!
-    
-    
+
     let minWordLength = 7
     var lives = 7 {
         didSet {
@@ -27,43 +26,41 @@ class ViewController: UIViewController {
         }
     }
     let alphabet = (97...122).map {String(UnicodeScalar($0))}
-    
+
     var textField: UITextField!
     var textFieldKeyboardConstraint: NSLayoutConstraint!
     var logoLabelKeyboardConstraint: NSLayoutConstraint!
-    
+
     override var preferredStatusBarStyle: UIStatusBarStyle { .lightContent }
-    
+
     override func loadView() {
         view = UIView()
-        
-        //MARK: - Background View
+
+        // MARK: - Background View
         let gradientStartColor = hexStringToUIColor(hex: "#022e57")
         let gradientEndColor = hexStringToUIColor(hex: "#005a8d")
-        
+
         let backgroundView = GradientView(gradientStartColor: gradientStartColor, gradientEndColor: gradientEndColor)
         backgroundView.translatesAutoresizingMaskIntoConstraints = false
-        
+
         view.addSubview(backgroundView)
-        
-        
-        //MARK: - Logo Label
+
+        // MARK: - Logo Label
         let logoLabel = UILabel()
         logoLabel.translatesAutoresizingMaskIntoConstraints = false
-        
+
         let logoLabelColor = hexStringToUIColor(hex: "#fff5fd")
         logoLabel.textColor = logoLabelColor
         logoLabel.textAlignment = .center
         logoLabel.font = UIFont.systemFont(ofSize: 24)
         logoLabel.text = "Hangman Game"
-        
+
         view.addSubview(logoLabel)
-        
-        
-        //MARK: - Text Field
+
+        // MARK: - Text Field
         textField = UITextField()
         textField.translatesAutoresizingMaskIntoConstraints = false
-        
+
         let textFieldColor = hexStringToUIColor(hex: "#fff5fd")
         textField.textColor = textFieldColor
         textField.textAlignment = .center
@@ -71,51 +68,50 @@ class ViewController: UIViewController {
         textField.tintColor = .clear
         textField.font = UIFont.systemFont(ofSize: 36)
         textField.keyboardType = .alphabet
-        
+
         view.addSubview(textField)
-        
-        
-        //MARK: - Scores Label
+
+        // MARK: - Scores Label
         scoresLabel = UILabel()
         scoresLabel.translatesAutoresizingMaskIntoConstraints = false
-        
+
         let scoresLabelColor = hexStringToUIColor(hex: "#fff5fd")
         scoresLabel.textColor = scoresLabelColor
         scoresLabel.textAlignment = .right
         scoresLabel.font = UIFont.systemFont(ofSize: 16)
         scoresLabel.layer.cornerRadius = 5
-        
+
         view.addSubview(scoresLabel)
-        
-        //MARK: - Constraints
+
+        // MARK: - Constraints
         textFieldKeyboardConstraint = textField.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor)
-        
+
         NSLayoutConstraint.activate([
-            
-            //MARK: For Background View
+
+            // MARK: For Background View
             backgroundView.topAnchor.constraint(equalTo: view.topAnchor),
             backgroundView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
             backgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             backgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            
-            //MARK: For Text View
+
+            // MARK: For Text View
             textField.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             textField.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             textField.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
             textFieldKeyboardConstraint,
-            
-            //MARK: For Logo Label
+
+            // MARK: For Logo Label
             logoLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             logoLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             logoLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             logoLabel.bottomAnchor.constraint(equalTo: textField.topAnchor),
-            
-            //MARK: For Scores Label
+
+            // MARK: For Scores Label
             scoresLabel.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
             scoresLabel.topAnchor.constraint(equalTo: view.layoutMarginsGuide.topAnchor)
         ])
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         NotificationCenter.default.addObserver(
@@ -123,44 +119,43 @@ class ViewController: UIViewController {
             selector: #selector(keyboardNotification(notification:)),
             name: UIResponder.keyboardWillChangeFrameNotification,
             object: nil)
-        
+
         textField.becomeFirstResponder()
         textField.delegate = self
-        
+
         performSelector(inBackground: #selector(startGame), with: nil)
     }
-    
+
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
-    
+
     @objc private func startGame() {
         goodAttempts = []
         failedAttempts = []
-        
+
         loadWords()
-        
+
         words!.shuffle()
-        
+
         for word in words! {
             if word.count >= minWordLength && !word.contains("-") {
                 keyWord = word.uppercased()
                 break
             }
         }
-        
+
         lives = keyWord!.count
-        
-        
+
         DispatchQueue.main.async {
             [weak self] in
             guard let self = self else { return }
-            
+
             self.textField.text = String(repeating: "?", count: self.keyWord!.count)
             self.textField.becomeFirstResponder()
         }
     }
-    
+
     private func loadWords() {
         do {
             if let path = Bundle.main.path(forResource: "3000words", ofType: "txt") {
@@ -171,44 +166,44 @@ class ViewController: UIViewController {
             fatalError(error.localizedDescription)
         }
     }
-    
+
     @objc private func keyboardNotification(notification: NSNotification) {
         guard let userInfo = notification.userInfo else { return }
-        
+
         if let keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
             let keyboardPosition = keyboardFrame.origin.y
-            
+
             if keyboardPosition >= UIScreen.main.bounds.size.height {
                 self.textFieldKeyboardConstraint.constant = 0.0
             } else {
                 self.textFieldKeyboardConstraint.constant = keyboardFrame.size.height * -0.35
             }
         }
-        
+
         UIView.animate(
             withDuration: 0.2,
             animations: { self.view.layoutIfNeeded() },
             completion: nil)
     }
-    
+
     @objc private func buttonTapped(sender: UIButton) {
         sender.isHidden = true
     }
-    
-    private func hexStringToUIColor (hex:String) -> UIColor {
-        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-        
-        if (cString.hasPrefix("#")) {
+
+    private func hexStringToUIColor (hex: String) -> UIColor {
+        var cString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if cString.hasPrefix("#") {
             cString.remove(at: cString.startIndex)
         }
-        
-        if ((cString.count) != 6) {
+
+        if (cString.count) != 6 {
             return UIColor.gray
         }
-        
-        var rgbValue:UInt64 = 0
+
+        var rgbValue: UInt64 = 0
         Scanner(string: cString).scanHexInt64(&rgbValue)
-        
+
         return UIColor(
             red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
             green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
@@ -216,24 +211,24 @@ class ViewController: UIViewController {
             alpha: CGFloat(1.0)
         )
     }
-    
+
     func doNextMove(with characterString: String) {
         let userChar = characterString.uppercased()
-        
+
         if goodAttempts.contains(userChar) || failedAttempts.contains(userChar) {
             return
         }
-        
+
         guard let keyWord = keyWord else { fatalError("Don't have any words to guess") }
-        
+
         if keyWord.contains(userChar) {
             goodAttempts.append(userChar)
-            
+
             var userWord = ""
-            
+
             for char in keyWord {
                 let letter = String(char)
-                
+
                 if letter == userChar || goodAttempts.contains(letter) {
                     userWord += letter
                 } else {
@@ -246,9 +241,9 @@ class ViewController: UIViewController {
             }
         } else {
             lives -= 1
-            
+
             failedAttempts.append(userChar)
-            
+
             UIView.transition(with: scoresLabel, duration: 0.5, options: .transitionCrossDissolve) {
                 self.scoresLabel.backgroundColor = self.hexStringToUIColor(hex: "#ff96ad")
             } completion: { _ in
@@ -257,53 +252,50 @@ class ViewController: UIViewController {
                 }
             }
 
-            
-            
             if lives == 0 {
                 gameOver()
             }
         }
-        
-        
+
     }
-    
+
     func gameOver() {
         let ac = UIAlertController(title: "YOU DIED", message: "It was \(keyWord!) 😵\nWant to try again?", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "OK", style: .default) {
             [weak self] _ in
             self?.startGame()
         })
-        
+
         present(ac, animated: true)
     }
-    
+
     func missionAccomplished() {
         let ac = UIAlertController(title: "Well done!", message: "It was \(keyWord!) 🕵️\nWant to play again?", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "Sure", style: .default) {
             [weak self] _ in
             self?.startGame()
         })
-        
+
         present(ac, animated: true)
     }
-    
+
 }
 
-//MARK: - UITextFieldDelegate
+// MARK: - UITextFieldDelegate
 extension ViewController: UITextFieldDelegate {
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        if (keyWord != nil) {
+        if keyWord != nil {
             return true
         }
         return false
     }
-    
+
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let characterString = string.lowercased()
         if alphabet.contains(characterString) {
             doNextMove(with: characterString)
         }
-        
+
         return false
     }
 }
