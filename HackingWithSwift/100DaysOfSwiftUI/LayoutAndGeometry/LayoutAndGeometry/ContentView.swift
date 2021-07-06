c357845b513726577b92138ce4d21527f730cf7f
Andrei Chenchik <andrei@chenchik.me>
2021-07-06 22:19:37 +0200

    HWS: Complete LayoutAndGeometry project

---
+++ HackingWithSwift/100DaysOfSwiftUI/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Andrei Chenchik on 6/7/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    let colors: [Color] = [.red, .green, .blue, .orange, .pink, .purple, .yellow]
+
+    var body: some View {
+        GeometryReader { fullView in
+            ScrollView(.vertical) {
+                ForEach(0..<50) { index in
+                    GeometryReader { geo in
+                        Text("Row #\(index)")
+                            .font(.title)
+                            .frame(width: fullView.size.width)
+                            .background(self.colors[index % 7])
+                            .rotation3DEffect(.degrees(Double(geo.frame(in: .global).minY - fullView.size.height / 2) / 5), axis: (x: 0, y: 1, z: 0))
+
+
+                    }
+                    .frame(height: 40)
+                }
+            }
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
