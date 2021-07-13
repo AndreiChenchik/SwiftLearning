3a4ada614fe0ab4dedd449752567f68773a53fdd
Andrei Chenchik <andrei@chenchik.me>
2021-07-13 15:35:18 +0200

    HWS+: Init UltimatePortfolio App, add Icon and DataController

---
+++ HackingWithSwift/UltimatePortfolio/UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,25 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Andrei Chenchik on 13/7/21.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    @StateObject var dataController: DataController
+    
+    init() {
+        let dataController = DataController()
+        _dataController = StateObject(wrappedValue: dataController)
+    }
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
+                .environmentObject(dataController)
+        }
+    }
+}
