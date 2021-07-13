a150353a90ae147dd4d7ba0b5f68e4c941c691d7
Andrei Chenchik <andrei@chenchik.me>
2021-07-13 19:04:58 +0200

    HWS+: Add item editing to UltimatePortfolio

---
+++ HackingWithSwift/UltimatePortfolio/UltimatePortfolio/ItemRowView.swift
@@ -0,0 +1,24 @@
+//
+//  ItemRowView.swift
+//  UltimatePortfolio
+//
+//  Created by Andrei Chenchik on 13/7/21.
+//
+
+import SwiftUI
+
+struct ItemRowView: View {
+    @ObservedObject var item: Item
+    
+    var body: some View {
+        NavigationLink(destination: EditItemView(item: item)) {
+            Text(item.itemTitle)
+        }
+    }
+}
+
+struct ItemRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRowView(item: Item.example)
+    }
+}
