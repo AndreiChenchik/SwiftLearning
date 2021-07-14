4e25a6aff47bc6b9bf0893cdc92cfec11ce95b9c
Andrei Chenchik <andrei@chenchik.me>
2021-07-14 17:30:41 +0200

    HWS+UP: Add Home Screen

---
+++ HackingWithSwift/UltimatePortfolio/UltimatePortfolio/Color-Additions.swift
@@ -0,0 +1,13 @@
+//
+//  Color-Additions.swift
+//  UltimatePortfolio
+//
+//  Created by Andrei Chenchik on 14/7/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let systemGroupedBackground = Color(UIColor.systemGroupedBackground)
+    static let secondarySystemGroupedBackground = Color(UIColor.secondarySystemGroupedBackground)
+}
