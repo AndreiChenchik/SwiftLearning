fc1ae9d764bb24a96c02c5041a97e7762134a798
Andrei Chenchik <andrei@chenchik.me>
2021-07-14 14:56:45 +0200

    HWS+UP: Implement Awards struct from JSON and AwardsView

---
+++ HackingWithSwift/UltimatePortfolio/UltimatePortfolio/Award.swift
@@ -0,0 +1,21 @@
+//
+//  Award.swift
+//  UltimatePortfolio
+//
+//  Created by Andrei Chenchik on 14/7/21.
+//
+
+import Foundation
+
+struct Award: Decodable, Identifiable {
+    var id: String { name }
+    let name: String
+    let description: String
+    let color: String
+    let criterion: String
+    let value: Int
+    let image: String
+    
+    static let allAwards = Bundle.main.decode([Award].self, from: "Awards.json")
+    static let example = allAwards[0]
+}
