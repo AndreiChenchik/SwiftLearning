7671292921637db24fc1305067c3fdc4ec1ae028
Andrei Chenchik <andrei@chenchik.me>
2021-07-14 16:48:55 +0200

    HWS+UP: Add Awards counting and descriptions to the UI

--- HackingWithSwift/UltimatePortfolio/UltimatePortfolio/AwardsView.swift
+++ HackingWithSwift/UltimatePortfolio/UltimatePortfolio/AwardsView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct AwardsView: View {
     static let tag: String? = "Awards"
     
+    @EnvironmentObject var dataController: DataController
+    @State private var selectedAward = Award.example
+    @State private var showingAwardDetails = false
+    
     var columns: [GridItem] {
         [GridItem(.adaptive(minimum: 100, maximum: 100))]
     }
@@ -20,20 +24,28 @@ struct AwardsView: View {
                 LazyVGrid(columns: columns) {
                     ForEach(Award.allAwards) { award in
                         Button {
-                            
+                            selectedAward = award
+                            showingAwardDetails = true
                         } label: {
                             Image(systemName: award.image)
                                 .resizable()
                                 .scaledToFit()
                                 .padding()
                                 .frame(width: 100, height: 100)
-                                .foregroundColor(Color.secondary.opacity(0.5))
+                                .foregroundColor(dataController.hasEarned(award: award) ? Color(award.color) : Color.secondary.opacity(0.5))
                         }
                     }
                 }
             }
             .navigationTitle("Awards")
         }
+        .alert(isPresented: $showingAwardDetails, content: {
+            if dataController.hasEarned(award: selectedAward) {
+                return Alert(title: Text("Unlocked: \(selectedAward.name)"), message: Text(selectedAward.description), dismissButton: .default(Text("OK")))
+            } else {
+                return Alert(title: Text("Locked"), message: Text(selectedAward.description), dismissButton: .default(Text("OK")))
+            }
+        })
     }
 }
 
