dc6d4df3ad8dbd4aae274c0a00834127063b19cb
Andrei Chenchik <andrei@chenchik.me>
2021-07-14 11:09:50 +0200

    HWS+UP: Add sorting settings and extend Sequence sorting options

--- HackingWithSwift/UltimatePortfolio/UltimatePortfolio/Project-CoreDataHelpers.swift
+++ HackingWithSwift/UltimatePortfolio/UltimatePortfolio/Project-CoreDataHelpers.swift
@@ -23,9 +23,11 @@ extension Project {
     }
     
     var projectItems: [Item] {
-        let itemsArray = items?.allObjects as? [Item] ?? []
-        
-        return itemsArray.sorted { first, second in
+        items?.allObjects as? [Item] ?? []
+    }
+    
+    var projectItemsDefaultSorted: [Item] {
+        projectItems.sorted { first, second in
             if first.completed == false {
                 if second.completed == true {
                     return true
@@ -66,5 +68,16 @@ extension Project {
         project.creationDate = Date()
         
         return project
+    }
+        
+    func projectItems(using sortOrder: Item.SortOrder) -> [Item] {
+        switch sortOrder {
+        case .optimized:
+            return projectItemsDefaultSorted
+        case .title:
+            return projectItems.sorted(by: \Item.itemTitle)
+        case .creationDate:
+            return projectItems.sorted(by: \Item.itemCreationDate)
+        }
     }
 }
---
+++ HackingWithSwift/UltimatePortfolio/UltimatePortfolio/Sequence-Sorting.swift
@@ -0,0 +1,49 @@
+//
+//  Sequence-Sorting.swift
+//  UltimatePortfolio
+//
+//  Created by Andrei Chenchik on 14/7/21.
+//
+
+import Foundation
+
+extension Sequence {
+    func sorted<Value>(by keyPath: KeyPath<Element, Value>, using areInIncreasingOrder: (Value, Value) throws -> Bool) rethrows -> [Element] {
+        try self.sorted {
+            try areInIncreasingOrder($0[keyPath: keyPath], $01[keyPath: keyPath])
+        }
+    }
+    
+    func sorted<Value: Comparable>(by keyPath: KeyPath<Element, Value>, as: Value.Type? = nil) -> [Element] {
+        return self.sorted(by: keyPath, using: <)
+    }
+    
+    func sorted(by keyPath: PartialKeyPath<Element>) -> [Element] {
+        guard let keyPathString = keyPath._kvcKeyPathString else { return Array(self) }
+        let sortDescriptor = NSSortDescriptor(key: keyPathString, ascending: true)
+        return self.sorted(by: sortDescriptor)
+    }
+    
+    func sorted(by sortDescriptor: NSSortDescriptor) -> [Element] {
+        self.sorted {
+            sortDescriptor.compare($0, to: $1) == .orderedAscending
+        }
+    }
+    
+    func sorted(by sortDescriptors: [NSSortDescriptor]) -> [Element] {
+        self.sorted {
+            for descriptor in sortDescriptors {
+                switch descriptor.compare($0, to: $1) {
+                case .orderedAscending:
+                    return true
+                case .orderedDescending:
+                    return false
+                case .orderedSame:
+                    continue
+                }
+            }
+            
+            return false 
+        }
+    }
+}
