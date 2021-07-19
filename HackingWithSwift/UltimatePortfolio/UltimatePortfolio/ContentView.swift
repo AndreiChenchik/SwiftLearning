45b1a3e9bd1df84a4179ee6e47ac311973cbac1b
Andrei Chenchik <andrei@chenchik.me>
2021-07-19 19:26:21 +0200

    HWS+UP: Add Siri support for shortcut

--- HackingWithSwift/UltimatePortfolio/UltimatePortfolio/ContentView.swift
+++ HackingWithSwift/UltimatePortfolio/UltimatePortfolio/ContentView.swift
@@ -12,6 +12,8 @@ struct ContentView: View {
     @SceneStorage("selectedView") var selectedView: String?
     @EnvironmentObject var dataController: DataController
 
+    private let newProjectActivity = "me.chenchik.ios.UltimatePortfolio.newProject"
+
     var body: some View {
         TabView(selection: $selectedView) {
             HomeView(dataController: dataController)
@@ -43,6 +45,11 @@ struct ContentView: View {
                 }
         }
         .onContinueUserActivity(CSSearchableItemActionType, perform: moveToHome)
+        .onContinueUserActivity(newProjectActivity, perform: createProject)
+        .userActivity(newProjectActivity) { activity in
+            activity.title = "New Project"
+            activity.isEligibleForPrediction = true
+        }
         .onOpenURL(perform: openURL)
     }
 
@@ -51,6 +58,11 @@ struct ContentView: View {
     }
 
     func openURL(_ url: URL) {
+        selectedView = ProjectsView.openTag
+        dataController.addProject()
+    }
+
+    func createProject(_ userActivity: NSUserActivity) {
         selectedView = ProjectsView.openTag
         dataController.addProject()
     }
