50c51a011542a621d9568fad4868e42d461e1501
Andrei Chenchik <andrei@chenchik.me>
2021-07-28 18:20:46 +0200

    HWS+UP: Add comments to shared projects

--- HackingWithSwift/UltimatePortfolio/UltimatePortfolio/Activities/Community/SharedItemsView.swift
+++ HackingWithSwift/UltimatePortfolio/UltimatePortfolio/Activities/Community/SharedItemsView.swift
@@ -14,6 +14,35 @@ struct SharedItemsView: View {
     @State private var items = [SharedItem]()
     @State private var itemsLoadState = LoadState.inactive
 
+    @State private var messages = [ChatMessage]()
+    @State private var messagesLoadState = LoadState.inactive
+
+    @AppStorage("username") var username: String?
+    @State private var showingSignIn = false
+    @State private var newChatText = ""
+
+    @ViewBuilder var messagesFooter: some View {
+        if username == nil {
+            Button("Sign in to comment", action: signIn)
+                .frame(maxWidth: .infinity)
+        } else {
+            VStack {
+                TextField("Enter your message", text: $newChatText)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .textCase(nil)
+
+                Button(action: sendChatMessage) {
+                    Text("Send")
+                        .frame(maxWidth: .infinity, minHeight: 44)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .clipShape(Capsule())
+                        .contentShape(Capsule())
+                }
+            }
+        }
+    }
+
     var body: some View {
         List {
             Section {
@@ -35,10 +64,27 @@ struct SharedItemsView: View {
                     }
                 }
             }
+
+            Section(
+                header: Text("Chat about this project…"),
+                footer: messagesFooter
+            ) {
+                if messagesLoadState == .success {
+                    ForEach(messages) { message in
+                        Text("\(Text(message.from).bold()): \(message.text)")
+                            .multilineTextAlignment(.leading)
+                    }
+                }
+            }
+
         }
         .listStyle(InsetGroupedListStyle())
         .navigationTitle(project.title)
-        .onAppear(perform: fetchSharedItems)
+        .onAppear {
+            fetchSharedItems()
+            fetchChatMessages()
+        }
+        .sheet(isPresented: $showingSignIn, content: SignInView.init)
     }
 
     func fetchSharedItems() {
@@ -76,6 +122,65 @@ struct SharedItemsView: View {
         }
 
         CKContainer.default().publicCloudDatabase.add(operation)
+    }
+
+    func fetchChatMessages() {
+        guard messagesLoadState == .inactive else { return }
+        messagesLoadState = .loading
+
+        let recordID = CKRecord.ID(recordName: project.id)
+        let reference = CKRecord.Reference(recordID: recordID, action: .none)
+        let predicate = NSPredicate(format: "project == %@", reference)
+        let sort = NSSortDescriptor(key: "creationDate", ascending: true)
+        let query = CKQuery(recordType: "Message", predicate: predicate)
+        query.sortDescriptors = [sort]
+
+        let operation = CKQueryOperation(query: query)
+        operation.desiredKeys = ["from", "text"]
+
+        operation.recordFetchedBlock = { record in
+            let message = ChatMessage(from: record)
+            messages.append(message)
+            messagesLoadState = .success
+        }
+
+        operation.queryCompletionBlock = { _, _ in
+            if messages.isEmpty {
+                messagesLoadState = .noResults
+            }
+        }
+
+        CKContainer.default().publicCloudDatabase.add(operation)
+    }
+
+    func signIn() {
+        showingSignIn = true
+    }
+
+    func sendChatMessage() {
+        let text = newChatText.trimmingCharacters(in: .whitespacesAndNewlines)
+        guard text.count > 2 else { return }
+        guard let username = username else { return }
+
+        let message = CKRecord(recordType: "Message")
+        message["from"] = username
+        message["text"] = text
+
+        let projectID = CKRecord.ID(recordName: project.id)
+        message["project"] = CKRecord.Reference(recordID: projectID, action: .deleteSelf)
+
+        let backupChatText = newChatText
+        newChatText = ""
+
+        CKContainer.default().publicCloudDatabase.save(message) { record, error in
+            if let error = error {
+                print(error.localizedDescription)
+                newChatText = backupChatText
+            } else if let record = record {
+                let message = ChatMessage(from: record)
+                messages.append(message)
+            }
+        }
     }
 }
 
