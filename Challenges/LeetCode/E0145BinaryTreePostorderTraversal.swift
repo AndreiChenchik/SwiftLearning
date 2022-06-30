8989b87e6b2fb1404b0b88076b5cfaf3062214ae
Andrei Chenchik <andrei@chenchik.me>
2022-06-30 17:56:58 +0200

    leetcode sp, data structures, level 1, day 10 3/3

---
+++ Challenges/LeetCode/E0145BinaryTreePostorderTraversal.swift
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func postorderTraversal(_ root: TreeNode?) -> [Int] {
+        var results = [Int]()
+        
+        if let node = root {
+            results += postorderTraversal(root?.left)
+            results += postorderTraversal(root?.right)
+            results.append(node.val)
+        }
+        
+        return results
+    }
+}
