69c1a7fd720ebb953ab81a3ca2170072a7f36030
Andrei Chenchik <andrei@chenchik.me>
2022-08-07 12:12:40 +0200

    Add Calc project for learning AutoLayout with Practicum

---
+++ Practicum/Sprint3/PracticumCalc/PracticumCalc/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticumCalc
+//
+//  Created by Andrei Chenchik on 3/8/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
