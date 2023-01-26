752869c637666e930ea74db684a3292b841a733e
Andrei Chenchik <andrei@chenchik.me>
2023-01-26 22:03:33 +0100

    Explore collection views

--- Practicum/EmojiMixer/EmojiMixer/ViewController.swift
+++ Practicum/EmojiMixer/EmojiMixer/ViewController.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private let emojis = [
+    private var emojis: [String] = []
+    private let possibleEmojis = [
         "🍇", "🍈", "🍉", "🍊", "🍋", "🍌", "🍍", "🥭", "🍎", "🍏", "🍐", "🍒", "🍓",
         "🫐", "🥝", "🍅", "🫒", "🥥", "🥑", "🍆", "🥔", "🥕", "🌽", "🌶️", "🫑", "🥒",
         "🥬", "🥦", "🧄", "🧅", "🍄"
@@ -19,11 +20,31 @@ class ViewController: UIViewController {
         collectionViewLayout: UICollectionViewFlowLayout()
     )
 
+    private let undoButton: UIButton = {
+        let button = UIButton()
+        
+        button.setTitle("Undo", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+
+        return button
+    }()
+
+    private let addButton: UIButton = {
+        let button = UIButton()
+
+        button.setTitle("+", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
 
-        configureCollectionView()
+        configureCollection()
+        configureViews()
+        configureActions()
     }
 }
 
@@ -48,22 +69,69 @@ class EmojiCell: UICollectionViewCell {
 }
 
 private extension ViewController {
-    func configureCollectionView() {
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-
+    func configureCollection() {
         collectionView.register(EmojiCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.dataSource = self
+        collectionView.delegate = self
+    }
 
+    func configureViews() {
+        let hStack = UIStackView()
+        let hStackMargins = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 20)
+        hStack.layoutMargins = hStackMargins
+        hStack.isLayoutMarginsRelativeArrangement = true
+        hStack.axis = .horizontal
+        hStack.distribution = .fill
+        hStack.alignment = .center
+        hStack.translatesAutoresizingMaskIntoConstraints = false
+
+        hStack.addArrangedSubview(undoButton)
+        hStack.addArrangedSubview(UIView())
+        hStack.addArrangedSubview(addButton)
+
+        view.addSubview(hStack)
+
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(collectionView)
 
         NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            hStack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            hStack.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            hStack.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.topAnchor.constraint(equalTo: hStack.bottomAnchor),
             collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
+    }
 
-        collectionView.dataSource = self
-        collectionView.delegate = self
+    func configureActions() {
+        addButton.addTarget(self, action: #selector(add), for: .touchUpInside)
+        undoButton.addTarget(self, action: #selector(undo), for: .touchUpInside)
+    }
+}
+
+extension ViewController {
+    @objc func add() {
+        guard let emoji = possibleEmojis.randomElement() else { return }
+
+        let count = emojis.count
+        emojis.append(emoji)
+
+        collectionView.performBatchUpdates {
+            collectionView.insertItems(at: [.init(row: count, section: 0)])
+        }
+    }
+
+    @objc func undo() {
+        guard emojis.count > 0 else { return }
+
+        emojis.removeLast()
+        let count = emojis.count
+
+        collectionView.performBatchUpdates {
+            collectionView.deleteItems(at: [.init(row: count, section: 0)])
+        }
     }
 }
 
