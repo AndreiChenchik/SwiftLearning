a09c1c5cd30bd1354638ac72935d402a2de17451
Andrei Chenchik <andrei@chenchik.me>
2023-02-11 11:42:42 +0100

    Add CoreData to EmojiMixer

--- Practicum/EmojiMixer/EmojiMixer/AppDelegate.swift
+++ Practicum/EmojiMixer/EmojiMixer/AppDelegate.swift
@@ -6,11 +6,20 @@
 //
 
 import UIKit
+import CoreData
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    lazy var persistentContainer: NSPersistentContainer = {                     // 1
+        let container = NSPersistentContainer(name: "Model")              // 2
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in // 3
+            if let error = error as NSError? {                              // 4
+                // Код для обработки ошибки ошибки
+            }
+        })
+        return container
+    }()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
