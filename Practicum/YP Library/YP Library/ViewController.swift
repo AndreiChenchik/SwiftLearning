4d88a3230e205e6fdc554d63ef5818c8d0242fd3
Andrei Chenchik <andrei@chenchik.me>
2023-02-11 11:40:31 +0100

    Playing with CoreData

---
+++ Practicum/YP Library/YP Library/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  YP Library
+//
+//  Created by Andrei Chenchik on 11/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
